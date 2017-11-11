20e7df7e90c7d02ba84eaec6c343c2f5bf1cc3ab
hpauzer <heatherpauzer@Heathers-MacBook-Pro-2.local>
2017-11-11 12:28:29 -0600

    removed duplicate connection.js

--- config/orm.js
+++ config/orm.js
@@ -5,9 +5,9 @@
 var connection = require('./connection.js');
 
 var orm = {
-    all: function(tableInput, cb){
-        connection.query('SELECT * FROM ' + tableInput + ';', function(err, result){
-            if(err) throw err;
+    all: function(tableInput, cb) {
+        connection.query('SELECT * FROM ' + tableInput + ';', function(err, result) {
+            if (err) throw err;
             cb(result)
         })
 
